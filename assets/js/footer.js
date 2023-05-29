3fe8b05a9e86a042466b63bd865cd42fc3c7707e
RDMartucci <dariomartucci@gmail.com>
2023-05-28 21:06:21 -0300

    footer con DOM

---
+++ assets/js/footer.js
@@ -0,0 +1,39 @@
+/** FOOTER del SITIO */
+/*const footerSite = '<p>&copy 2023 todos los derechos reservados</p>';
+document.querySelector('#footer').innerHTML = footerSite;
+*/
+/*
+const footerSiteNUEVO = `
+<div class="contenedor-footer-hijo1">
+            <a href="https://www.twitter.com" class="iconos-sociales" target="_blank">
+                <i class="fa-brands fa-twitter"></i>
+            </a>
+        </div>
+        <div class="contenedor-footer-hijo2">
+            <a href="https://www.facebook.com" class="iconos-sociales" target="_blank">
+                <i class="fa-brands fa-facebook"></i>
+            </a>
+        </div>
+        <div class="contenedor-footer-hijo3">
+            <a href="https://www.pinterest.com" class="iconos-sociales" target="_blank">
+                <i class="fa-brands fa-pinterest"></i>
+            </a>
+        </div>
+        <div class="contenedor-footer-hijo4">
+            <a href="https://www.instagram.com" class="iconos-sociales" target="_blank">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+        </div>
+        <div class="contenedor-footer-hijo5">
+            <a href="https://www.linkedin.com" class="iconos-sociales" target="_blank">
+                <i class="fa-brands fa-linkedin"></i>
+            </a>
+        </div>
+        <div class="contenedor-footer-hijo6">
+            <p>
+                <span class="parrafo-footer">@2023 Derechos Reservados </span>
+            </p>
+        </div>
+`;*/
+
+document.querySelector('#footer').innerHTML = footerSiteNUEVO;
